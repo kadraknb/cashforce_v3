266f9bd313a4ae1f8eb4d8f722d2f19d70ea867f
kadraknb <kadrak02@hotmail.com>
2023-01-01 05:30:49 -0300

    add seed

---
+++ back-end/src/database/seeders/20221228075453-buyers.js
@@ -0,0 +1,39 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert('buyers', [
+      {
+        id: 1,
+        name: 'SACADO 001',
+        tradingName: 'SACADO 001 LTDA',
+        cashforceTax: '0',
+        responsibleName: null,
+        responsibleEmail: null,
+        responsiblePosition: null,
+        responsiblePhone: null,
+        responsibleMobile: null,
+        website: null,
+        postalCode: null,
+        address: null,
+        number: null,
+        complement: null,
+        neighborhood: null,
+        city: null,
+        state: null,
+        phoneNumber: null,
+        situation: null,
+        situationDate: null,
+        createdAt: '2020-10-29 21:20:33',
+        updatedAt: '2020-10-29 21:20:34',
+        cnpjId: 1,
+        confirm: 1,
+        email: null,
+      },
+    ]);
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('buyers', null, {});
+  },
+};
