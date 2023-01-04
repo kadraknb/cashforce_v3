ee8004659a061666472d7050d811474dd733b299
kadraknb <kadrak02@hotmail.com>
2023-01-04 06:38:22 -0300

    ajuste

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -8,11 +8,22 @@ const app = express();
 
 app.use(express.json());
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'YOUR-DOMAIN.TLD'); // update to match the domain you will make the request from
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', 'YOUR-DOMAIN.TLD'); // update to match the domain you will make the request from
+//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//   next();
+// });
+
+const accessControl = (_req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Methods',
+    'GET,POST,DELETE,OPTIONS,PUT,PATCH',
+  );
+  res.header('Access-Control-Allow-Headers', '*');
   next();
-});
+};
+app.use(accessControl);
 
 app.use(routes);
 
