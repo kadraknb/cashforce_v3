2f619fd6035085687e78d2526290c902e4ba6203
kadraknb <kadrak02@hotmail.com>
2023-01-04 06:43:04 -0300

    ajuste

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -2,27 +2,13 @@ const express = require('express');
 require('express-async-errors');
 
 const errorMiddleware = require('./middlewares/erros');
+const accessControl = require('./middlewares/setHeader');
 const routes = require('./routes/router');
 
 const app = express();
 
 app.use(express.json());
 
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', 'YOUR-DOMAIN.TLD'); // update to match the domain you will make the request from
-//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-//   next();
-// });
-
-const accessControl = (_req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header(
-    'Access-Control-Allow-Methods',
-    'GET,POST,DELETE,OPTIONS,PUT,PATCH',
-  );
-  res.header('Access-Control-Allow-Headers', '*');
-  next();
-};
 app.use(accessControl);
 
 app.use(routes);
---
+++ back-end/src/middlewares/setHeader.js
@@ -0,0 +1,11 @@
+const accessControl = (_req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Methods',
+    'GET,POST,DELETE,OPTIONS,PUT,PATCH',
+  );
+  res.header('Access-Control-Allow-Headers', '*');
+  next();
+};
+
+module.exports = accessControl;
