94d9ec7bcb016c4ad971d5b0ae25779f5eeab2a6
kadraknb <kadrak02@hotmail.com>
2023-01-04 01:50:28 -0300

    add funcao para fortama o objeto do "getAllOrders"

---
+++ back-end/src/utils/formatNF.js
@@ -0,0 +1,28 @@
+const status = [
+  'Pendente de confirmação',
+  'Pedido confirmado',
+  'Não reconhece o pedido',
+  'Mercadoria não recebida',
+  'Recebida com avaria',
+  'Devolvida',
+  'Recebida com devolução parcial',
+  'Recebida e confirmada',
+  'Pagamento Autorizado',
+];
+
+const formatNF = (NF) => {
+  const data = NF.map((itemNF) => ({
+    nNotaFiscal: itemNF.nNf,
+    sacado: itemNF.buyerToOrders.name,
+    cedente: itemNF.providerToOrders.name,
+    emissão: new Date(itemNF.emissionDate).toLocaleDateString(),
+    valor: Number(itemNF.value).toLocaleString('pt-BR', {
+      minimumFractionDigits: 2,
+    }),
+    status: status[itemNF.orderStatusBuyer],
+  }));
+
+  return data;
+};
+
+module.exports = { formatNF };
