c8fa67b91a6a443a93f56b4c2f163b6aca0da234
kadraknb <kadrak02@hotmail.com>
2023-01-04 06:11:52 -0300

    ajuste

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -8,6 +8,15 @@ const app = express();
 
 app.use(express.json());
 
+app.use((_req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'YOUR-DOMAIN.TLD');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept',
+  );
+  next();
+});
+
 app.use(routes);
 
 app.use(errorMiddleware);
