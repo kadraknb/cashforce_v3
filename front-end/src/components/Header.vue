8e235619656e557521fa63d003166f8eef82717b
kadraknb <kadrak02@hotmail.com>
2023-01-04 01:32:10 -0300

    add componente header

---
+++ front-end/src/components/Header.vue
@@ -0,0 +1,19 @@
+<script>
+</script>
+
+<template>
+  <header id="header">
+  </header>
+</template>
+
+<style>
+#header {
+  width: 100%;
+  height: 64px;
+  padding: 16px 46px;
+
+  background: #FFFFFF;
+  border-bottom: 1px #DFE2EB solid;
+  border-radius: 15px 0px 0px 0px;
+}
+</style>
