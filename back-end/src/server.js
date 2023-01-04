a247b05d761d23891c5e557088961447ac738a44
kadraknb <kadrak02@hotmail.com>
2023-01-04 03:40:14 -0300

    ajuste

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 require('dotenv').config();
 
-const port = process.env.API_PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.listen(port, () => console.log('ouvindo porta', port));
