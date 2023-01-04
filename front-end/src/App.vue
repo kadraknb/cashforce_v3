cb1cb7a688bd8bd1e119b6e7e4cb004cf953fa98
kadraknb <kadrak02@hotmail.com>
2023-01-04 01:40:05 -0300

    incorporando a "page" de NF

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,30 +1,34 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import PageDefaut from './components/PageDefault.vue'
+import AsiderFinanciador from './components/AsideFinanciador.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div id="pageMain">
+    <AsiderFinanciador />
+    <div>
+      <PageDefaut />
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+:root {
+  --PDarkBlue5: #021B51;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+
+#pageMain {
+  display: flex;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+#pageMain div {
+  width: 100%;
+  height: 100vh;
+}
+
+@media (max-width: 922px) {
+  #pageMain div {
+    width: auto;
+  }
 }
 </style>
